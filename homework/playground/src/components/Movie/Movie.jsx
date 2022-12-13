181b9b85e07522ea782d2ba4a419fbaa851bd4c8
gheo <gheorghe.coanta@gmail.com>
2022-12-13 20:52:23 +0200

    Add Homework 01 - part 2
    Add login functionality with in memory users.

--- homework/playground/src/components/Movie/Movie.jsx
+++ homework/playground/src/components/Movie/Movie.jsx
@@ -3,7 +3,12 @@ import './Movie.css';
 function Movie(props) {
     return <div className="movie-preview" key={props.id}>
         <h2>{props.name}</h2>
-        <p>Written by {props.director}</p>
+        <p>Directed by {props.director}</p>
+        <iframe width="420" height="315"
+                src={props.trailer} title="YouTube video player" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen>
+        </iframe>
     </div>
 }
 
--- homework/playground/src/containers/App.jsx
+++ homework/playground/src/containers/App.jsx
@@ -1,29 +1,40 @@
 import Movie from "../components/Movie";
 import {useState} from "react";
 import '../components/Movie/Movie.css';
+import Login from "../components/Login";
 
 function App() {
     const [movies, setMovies] = useState([
-        {name: 'Gladiator', director: 'John', id: 1},
-        {name: 'Batman Returns', director: 'Will', id: 2},
-        {name: 'Lord of the rings', director: 'Bob', id: 3}
+        {name: 'Gladiator', director: 'John', id: 1, trailer:'https://www.youtube.com/embed/uvbavW31adA'},
+        {name: 'Batman Returns', director: 'Will', id: 2, trailer: 'https://www.youtube.com/embed/EXeTwQWrcwY'},
+        {name: 'Lord of the rings', director: 'Bob', id: 3, trailer: 'https://www.youtube.com/embed/N4xV2RIlMi4'}
     ]);
 
     const [userLogged, setUserLogged] = useState(false);
+    const [authenticatedUser, setAuthenticatedUser] = useState({username: 'visitor'});
 
     const authenticate = function (auth) {
         setUserLogged(auth);
+        setAuthenticatedUser({username: 'visitor'})
+    }
+
+    const authUser = function (u){
+        setAuthenticatedUser(u)
     }
 
     return <>
-        <h2>Hello George</h2>
-        <button onClick={()=> authenticate(true)}>Click me</button>
+        <h2>Hello {authenticatedUser?.username}</h2>
+
+        {!userLogged && (<Login authenticate={authenticate} setAuth={authUser}/>)}
 
         {userLogged && (
             <div className="movie-container">
                 {movies.map((movie) => <Movie key={movie.id} name={movie.name} director={movie.director}
-                                              id={movie.id}/>)}
-            </div>)}
+                                              trailer={movie.trailer}  id={movie.id}/>)} </div>)}
+        {userLogged && <h1>welcome {authenticatedUser?.username}</h1>}
+        {userLogged && (<button onClick={() => authenticate(false)}>Log out</button>)}
+
+        {userLogged && authenticatedUser?.role ==='admin' &&(<h2>Secret message for ADMIN'S only</h2>)}
     </>
 }
 
